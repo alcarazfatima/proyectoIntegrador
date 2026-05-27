34f0e214e3de8585ee2190b9e2b368b9cf6ac086
alcarazfatima <alcarazfatima@hotmail.com.ar>
2026-05-27 19:31:58 -0300

    Comentarios agregados a vista perfil y home

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,4 @@
-import { Comment } from "../models/Comment.js";
+import { Comment, Post, Notification } from "../models/sync.js";
 
 export const postCrearComentario = async (req, res) => {
 
@@ -10,17 +10,32 @@ export const postCrearComentario = async (req, res) => {
         const userId = req.session.user.id;
         const { postId, content } = req.body; // vienen del formulario de pug
 
-        if (!content || content.trim === '') {
+        if (!content || content.trim() === '') {
             return res.redirect('/home');
         }
 
-        await Comment.create({
+        const nuevoComentario = await Comment.create({
             content: content,
             postId: postId,
             userId: userId
         });
 
-        res.redirect('/home');
+        //busco la publicaicon para saber quien en el dueño del post (el receptor)
+        const publicacion = await Post.findByPk(postId);
+
+        if (publicacion) {
+            // solo cuando el comentario es de otro, no del dueño del post
+            if (parseInt(userId) !== parseInt(publicacion.userId)) {
+                await Notification.create({
+                    tipo: 'comentario',
+                    receptorId: publicacion.userId,
+                    actorId: userId,
+                    referenciaId: nuevoComentario.id
+                });
+            }
+        }
+
+        res.redirect(req.get('referer') || '/home');
 
     } catch (error) {
         console.error('Error al crear comentario', error)
