ca239a97b446b1b2ba133f69c87c252d9ad79c3a
alcarazfatima <alcarazfatima@hotmail.com.ar>
2026-06-02 16:47:31 -0300

    dialectModule pg y ssl para conexion en vercel agregado

--- models/config.js
+++ models/config.js
@@ -1,13 +1,21 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
+import pg from "pg"; // agrego para vercel
 
 const sequelize = new Sequelize({
     dialect: 'postgres',
+    dialectModule: pg, // y esto tambien para que vercel "encuentre pg"
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // esto es obligatorio para que Neon no  rechace la conexion
+        }
+    }
 });
 
 export default sequelize;
